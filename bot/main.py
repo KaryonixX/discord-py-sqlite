144266f8a6b253a78c1001980ef2b8fa4cfe7296
Audie Ni <audie.ni@yahoo.com>
2019-11-26 01:58:00 -0500

    Added code to connect to bot

---
+++ bot/main.py
@@ -0,0 +1,14 @@
+import os
+from discord.ext import commands
+
+bot = commands.Bot(
+    command_prefix="!"
+)
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+@bot.event
+async def on_ready():
+    print("Logged in as {}({})".format(bot.user.name, bot.user.id))
+
+bot.run(TOKEN)
